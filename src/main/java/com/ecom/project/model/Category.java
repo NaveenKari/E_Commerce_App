821da4f8c4019553e7e8112d4808d7e18e25765f
naveenkari <karinaveen0987@gmail.com>
2026-03-15 19:43:21 -0500

    added Validation dependency.

--- src/main/java/com/ecom/project/model/Category.java
+++ src/main/java/com/ecom/project/model/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,5 +18,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int categoryId;
+
+    @NotBlank
     private String categoryName;
 }
