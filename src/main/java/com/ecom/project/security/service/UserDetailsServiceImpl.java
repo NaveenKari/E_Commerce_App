19266965ca9b8b96e1402e181ddb85f8cad128b7
naveenkari <karinaveen0987@gmail.com>
2026-04-15 20:11:55 -0500

    Implemented Jwt and custom spring security config.

---
+++ src/main/java/com/ecom/project/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ecom.project.security.service;
+
+import com.ecom.project.model.User;
+import com.ecom.project.repo.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found With Username: "+ username));
+        return UserDetailsImpl.build(user);
+    }
+}
